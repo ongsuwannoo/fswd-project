a92a6fc757be71baa004ec6c388fad9ae25b1073
Zimbo333 <thanawat..kemwat@gmail.com>
2021-04-14 03:05:24 +0700

    :lipstick: register page form success only just icon left

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import "./App.css";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Register from "./Pages/Register";
-import Home from "./Pages/Home";
+import Register from "./Pages/Register/Register";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <Router>
-      <li>
-        <Link to="/Register">to Register</Link>
-      </li>
       <div>
         <Switch>
           <Route exact path="/">
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,7 +1,24 @@
-function Register (){
-    return(
-        <h1>From Regispage</h1>
-    )
+function Register() {
+  return (
+    <div className="container px-96 bg-gray-200">
+      <div className="flex justify-center">
+        <form>
+          <div className="grid grid-cols-12">
+            <h2 className="font-bold text-4xl col-span-12 text-center py-16">ลงทะเบียน</h2>
+            <p className="col-span-12" >ชื่อ</p>
+            <input className="col-span-12 mb-4 py-2 pl-4 pr-24" type="text" placeholder="กรอกชื่อ"></input>
+            <p className="col-span-12" >นามสกุล</p>
+            <input className="col-span-12 mb-4 py-2 pl-4 pr-24" type="text" placeholder="กรอกนามสกุล"></input>
+            <p className="col-span-12" >ชื่อผู้ใช้งาน</p>
+            <input className="col-span-12 mb-4 py-2 pl-4 pr-24" type="text" placeholder="กรอกชื่อผู้ใช้งาน"></input>
+            <p className="col-span-12" >รหัสผ่าน</p>
+            <input className="col-span-12 mb-4 py-2 pl-4 pr-24" type="text" placeholder="กรอกรหัสผ่าน"></input>
+            <button className="col-span-12 bg-black text-white py-2 my-4">ลงทะเบียน</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Register;

cc191be041235d6e3d201c006559acb83c275776
Jakkrapat Suwanno <ongsuwannoo@gmail.com>
2021-04-21 19:32:19 +0700

    🔥 Add backend-test for test API Graphql uploadFile

--- backend/src/graphql/mutations/product.js
+++ backend/src/graphql/mutations/product.js
@@ -1,3 +1,38 @@
 import { ProductTC } from '../../models'
+import { schemaComposer } from 'graphql-compose'
+import { GraphQLUpload } from 'apollo-upload-server';
+
+const path = require('path')
+const fs = require('fs')
+
+schemaComposer.add(GraphQLUpload);
 
 export const createProduct = ProductTC.getResolver('createOne')
+
+const UploadPayload = schemaComposer.createObjectTC({
+  name: 'UploadPayload',
+  fields: {
+    url: 'String!',
+  },
+})
+
+export const uploadFile = schemaComposer.createResolver({
+  name: "uploadFile",
+  args: {
+    file: 'Upload!'
+  },
+  type: UploadPayload,
+  resolve: async ({ args }) => {
+    const { createReadStream, filename, mimetype, mode } = await args.file.file
+    
+    const pathName = path.join(__dirname, `../../../public/images/${filename}`)
+    console.log(pathName);
+    await new Promise((res) =>
+      createReadStream().pipe(fs.createWriteStream(pathName)).on("close", res)
+    );
+
+    return {
+      url: `http://localhost:3001/images/${filename}`,
+    };
+  },
+})

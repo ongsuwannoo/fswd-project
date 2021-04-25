71b9000af1d4322b592a8ea7608f268ceb7ec58e
Jakkrapat Suwanno <ongsuwannoo@gmail.com>
2021-04-25 15:29:41 +0700

    🚨 SessionProvider Not working

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { SessionProvider } from './contexts/SessionContext'
 import reportWebVitals from './reportWebVitals';
 import './tailwind.output.css';
 
-const uri = 'http://103.13.231.22:3001/graphql'
+const uri = 'http://localhost:3001/graphql'
 const client = new ApolloClient({
   uri: uri,
   cache: new InMemoryCache({
@@ -19,7 +19,11 @@ const client = new ApolloClient({
   }),
   credentials: 'include',
   link: createUploadLink({
-    uri: uri
+    uri: uri,
+    cache: new InMemoryCache({
+      addTypename: false
+    }),
+    credentials: 'include',
   }),
 })
 

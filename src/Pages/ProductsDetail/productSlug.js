0febf940917ba4a0f989f86b9284e8e7d73929f1
aprilsrp <43024156+aprilsrp@users.noreply.github.com>
2021-04-30 22:23:09 +0700

    Update Link navbar

---
+++ src/Pages/ProductsDetail/productSlug.js
@@ -0,0 +1,9 @@
+import ProductCard from '../../components/productCard/ProductCard'
+function productSlug(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default productSlug;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,41 +1,41 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-function Sidebar(){
+function Sidebar() {
     const [email, setEmail] = useState('ongsuwannoo_255556@gmail.com')
     const checkEmail = () => {
         console.log(email)
-        if(email.length >= 25){
-            setEmail(email.substr(0,24) + '.....')
+        if (email.length >= 25) {
+            setEmail(email.substr(0, 24) + '.....')
         }
-        
+
     }
     useEffect(() => {
         checkEmail()
     })
-    
-    return(
-        <div id="admin_sidebar" className="bg-indigo-900 text-white">
+
+    return (
+        <div id="admin_sidebar" className="bg-gray-900 text-white">
             <h1 className="font-bold text-2xl py-8 text-center" >Marketry</h1>
             <h4 className="font-bold text-center">Jakkrapat Suwanno</h4>
             <h5 className="text-center text-xs">{email}</h5>
             <div className="mt-12">
-            <Link>
-                <div className="text-center py-2 my-2 hover:bg-blue-600 active:bg-blue-600"> 
-                <i class="fas fa-home"></i>  Home</div>
-            </Link>
-            <Link>
-                <div className="text-center py-2 my-2 hover:bg-blue-600 active:bg-blue-600">
-                <i class="fas fa-box"></i>  Product</div>
-            </Link>
-            <Link>
-                <div className="text-center py-2 my-2 hover:bg-blue-600 active:bg-blue-600">
-                <i class="fas fa-dollar-sign"></i>  Promotion</div>
-            </Link>
-            <Link>
-            <div className="text-center py-2 my-2 hover:bg-blue-600 active:bg-blue-600">
-            <i class="fas fa-shopping-cart"></i>  Order</div>
-            </Link>
+                <Link>
+                    <div className="rounded px-10 py-2 m-2 hover:bg-blue-600 active:bg-blue-600">
+                        <i class="fas fa-home"></i>  Home</div>
+                </Link>
+                <Link>
+                    <div className="rounded px-10 py-2 m-2 hover:bg-blue-600 active:bg-blue-600">
+                        <i class="fas fa-box"></i>  Product</div>
+                </Link>
+                <Link>
+                    <div className="rounded px-10 py-2 m-2 hover:bg-blue-600 active:bg-blue-600">
+                        <i class="fas fa-dollar-sign"></i>  Promotion</div>
+                </Link>
+                <Link>
+                    <div className="rounded px-10 py-2 m-2 hover:bg-blue-600 active:bg-blue-600">
+                        <i class="fas fa-shopping-cart"></i>  Order</div>
+                </Link>
             </div>
         </div>
     )

2a7f809628b0da0d857627222ad0a20a086c37b8
Jakkrapat Suwanno <ongsuwannoo@gmail.com>
2021-05-03 05:09:12 +0700

    🚨 Hotfix

--- backend/src/utils/generateRandomString.js
+++ backend/src/utils/generateRandomString.js
@@ -1,6 +1,6 @@
 export const generateRandomString = (length) => {
     var result = [];
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
         result.push(characters.charAt(Math.floor(Math.random() *

2f00f1747e612b9c4f5b1433ba0c8856f9b02db5
Zimbo333 <thanawat.kemwat@gmail.com>
2021-05-03 04:15:27 +0700

    pagination home

--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -1,14 +1,22 @@
 import icartadd from "../../icon/cart-plus-solid.svg"
 import { Link, Switch, useRouteMatch } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function ProductCard(props) {
   const onError = (e) => {
-    e.target.src = 'https://via.placeholder.com/398x500'
+    e.target.src = `https://picsum.photos/398/500/?${Math.random()}`
   }
+  const [image, setImage] = useState([])
+
+useEffect(()=>{
+  setImage(props.data.image)
+  console.log(props.data.image)
+},[setImage])
   return (
     <div className={props.numSpan}>
       <Link to={`/product/detail/${props.data?._id}`} onClick={()=>{window.location.href=`/product/detail/${props.data?._id}`}}>
-      <img src={`https://picsum.photos/398/500/?${Math.random()}`} onError={onError} className="" alt="productDetail" width="398" height="500"></img>
+      <div id="image_product" onError={onError} className="col-span-8 image_product" alt="showing" style={{ '--img': `url(${image[0]})` }}>
+              </div>
       <h3 className="px-2 text-xl lg:text-2xl">{props.data?.name}</h3>
       <h4 className="px-2 text-opacity-70">{props.data?.description}</h4>
       <h5 className="px-2 text-xl lg:text-2xl">{props.data?.price}</h5>
